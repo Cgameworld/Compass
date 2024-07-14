5d74e1412cc6d63f9fff7cee8dc987ce8d693373
Cgameworld <support@cgameworld.com>
2024-07-14 14:41:18 -0700

    smooth reset works

--- CompassUISystem.cs
+++ CompassUISystem.cs
@@ -1,7 +1,9 @@
 ﻿using Colossal.UI.Binding;
+using FirstPersonCamera.Helpers;
 using Game.Rendering;
 using Game.UI;
 using Game.UI.InGame;
+using System.Collections;
 using UnityEngine;
 
 namespace Compass
@@ -26,18 +28,38 @@ namespace Compass
         {
             if (Camera.main != null)
             {
-                var existingRotation = World.GetExistingSystemManaged<CameraUpdateSystem>().activeCameraController.rotation;
-                World.GetExistingSystemManaged<CameraUpdateSystem>().activeCameraController.rotation = new Vector3(existingRotation.x, 0f, existingRotation.z);
+                StaticCoroutine.Start(SmoothRotation());
             }
-            
+        }
+
+        private IEnumerator SmoothRotation()
+        {
+            var cameraUpdateSystem = World.GetExistingSystemManaged<CameraUpdateSystem>();
+            var existingRotation = cameraUpdateSystem.activeCameraController.rotation;
+            var targetRotation = new Vector3(existingRotation.x, 0f, existingRotation.z);
+            var smoothTime = 0.3f; 
+
+            float elapsedTime = 0;
+            while (elapsedTime < smoothTime)
+            {
+                cameraUpdateSystem.activeCameraController.rotation = Vector3.Lerp(existingRotation, targetRotation, (elapsedTime / smoothTime));
+                elapsedTime += World.Time.DeltaTime;
+                yield return null;
+            }
+
+            cameraUpdateSystem.activeCameraController.rotation = targetRotation;
         }
 
         protected override void OnUpdate()
         {
             if (Camera.main != null)
             {
-                rotation = World.GetExistingSystemManaged<CameraUpdateSystem>().activeCameraController.rotation.y;
-                rotationBinding.Update();
+                CameraUpdateSystem _cameraUpdateSystem = World.GetExistingSystemManaged<CameraUpdateSystem>();
+                if (_cameraUpdateSystem != null && _cameraUpdateSystem.activeCameraController != null)
+                {
+                    rotation = _cameraUpdateSystem.activeCameraController.rotation.y;
+                    rotationBinding.Update();
+                }
             }
         }
     }
